9a62ba3025ef037ea4fa76db38c231a94fa88038
nombre5ans <lewishw@uab.edu>
2013-06-24 03:09:45 +0000

    Commit of Chapter 6 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotApp::Application.config.secret_token = '38411b6b822052557bc739fc9cc41bf4214bd86d4ff494c24d107cbf9bc61b1e7cc02d15afe3500d9336a4d37f441382f13ad04d8a132995df52c837c78f125f'
